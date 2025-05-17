657153612edd876b771cc2959104d87e0a8b7a34
EvSecDev <dev@evsec.net>
2025-05-17 10:59:10 -0700

    Added last release commit tracking file

--- .build_helpers/check_for_dev_artifacts.sh
+++ .build_helpers/check_for_dev_artifacts.sh
@@ -8,6 +8,25 @@ function check_for_dev_artifacts() {
 
 	echo "[*] Checking for development artifacts in source code..."
 
+        # Get head commit hash
+        headCommitHash=$(git rev-parse HEAD)
+
+	# Get commit where last release was generated from
+	lastReleaseCommitHash=$(cat "$repoDir"/.last_release_commit)
+
+        # Retrieve the program version from the last release commit
+        lastReleaseVersionNumber=$(git show "$lastReleaseCommitHash":"$src"/main.go 2>/dev/null | grep "progVersion string" | cut -d" " -f5 | sed 's/"//g')
+
+        # Get the current version number
+        currentVersionNumber=$(grep "progVersion string" "$src"/main.go | cut -d" " -f5 | sed 's/"//g')
+
+        # Exit if version number hasn't been upped since last commit
+        if [[ $lastReleaseVersionNumber == $currentVersionNumber ]] && ! [[ $headCommitHash == $lastReleaseCommitHash ]] && [[ -n $lastReleaseVersionNumber ]]
+        then
+                echo -e "   ${RED}[-] ERROR${RESET}: Version number in $src/main.go has not been bumped since last commit, exiting build"
+                exit 1
+        fi
+
 	# Quick check for any left over debug prints
 	if grep -ER "DEBUG" "$src"/*.go
 	then
